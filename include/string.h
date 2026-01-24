7ef37894196d0c4dd8d4dfb1263befa69561807e
dask-58 <140686560+dask-58@users.noreply.github.com>
2026-01-24 16:30:42 +0530

    feat: string utils

---
+++ include/string.h
@@ -0,0 +1,11 @@
+#ifndef STRING_H
+#define STRING_H
+
+typedef unsigned long size_t;
+
+size_t strlen(const char *str);
+void *memset(void *dest, int val, size_t n);
+void *memcpy(void *dest, const void *src, size_t n);
+int strcmp(const char *s1, const char *s2);
+
+#endif
---
+++ kernel/string.c
@@ -0,0 +1,34 @@
+#include "../include/string.h"
+
+size_t strlen(const char* str) {
+    size_t len = 0;
+    while (str[len]) {
+        len++;
+    }
+    return len;
+}
+
+void* memset(void* dest, int val, size_t n) {
+    unsigned char* ptr = (unsigned char*)dest;
+    while (n--) {
+        *ptr++ = (unsigned char)val;
+    }
+    return dest;
+}
+
+void* memcpy(void* dest, const void* src, size_t n) {
+    unsigned char* d = (unsigned char*)dest;
+    const unsigned char* s = (const unsigned char*)src;
+    while (n--) {
+        *d++ = *s++;
+    }
+    return dest;
+}
+
+int strcmp(const char* s1, const char* s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return *(unsigned char*)s1 - *(unsigned char*)s2;
+}
